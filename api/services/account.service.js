32b8ecc799e1ed8598175c9b20e714e29f8e5fcf
qtt153759 <93815166+qtt153759@users.noreply.github.com>
2021-12-02 17:40:03 +0700

    Update Account.service
    áp dụng http error, và viết export.module theo es6

--- api/services/account.service.js
+++ api/services/account.service.js
@@ -1,100 +1,101 @@
-const Account= require("../models/account.model");
-const bcrypt =require ("bcryptjs")
-const salt=bcrypt.genSaltSync(10);
+const Account = require("../models/account.model");
+const bcrypt = require("bcryptjs");
+const createError = require("http-errors");
+const salt = bcrypt.genSaltSync(10);
 
-let handleUserLogin= (dataLogin)=>{
-    return new Promise(async(resolve,reject)=>{
-        try{
-            password=dataLogin.password;
-            email=dataLogin.email;
-            let userData={};
-            let isExsit=await checkUserEmail(email);
-            if(isExsit){
-                let user=await Account.findOne({
-                    attributes:['email','role','password','demographic_id'],//select
-                    where:{email:email},
-                    raw:true
-                });
-                if(user){
-                    let check=await bcrypt.compareSync(password,user.password);
-                    if(check){
-                        userData.errCode=0;
-                        userData.errMessage="Ok";
-                        delete user.password;//sau khi attributes ma muon an thuoc tinh nao thi delete
-                        userData.user=user;
-                    }else{
-                        userData.errCode=3;
-                        userData.errMessage="wrong password";
-                    }
-                }else{
-                    userData.errCode=2;
-                    userData.errMessage="User not found";
-                }
-            }else{
-                userData.errCode=1;
-                userData.errMessage="Your email isn't exsit"
-            }
-            resolve(userData);
-        }catch(er){
-            reject(er);
-        }
-    });
-}
+let handleUserLogin = (dataLogin) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			password = dataLogin.password;
+			email = dataLogin.email;
+			let userData = {};
+			let isExsit = await checkUserEmail(email);
+			if (isExsit) {
+				let user = await Account.findOne({
+					attributes: ["email", "role", "password", "demographic_id"], //select
+					where: { email: email },
+					raw: true,
+				});
+				if (user) {
+					let check = await bcrypt.compareSync(
+						password,
+						user.password
+					);
+					if (check) {
+						userData.errMessage = "Login successful";
+						delete user.password; //sau khi attributes ma muon an thuoc tinh nao thi delete
+						userData.user = user;
+					} else {
+						throw createError(500, "wrong password");
+					}
+				} else {
+					throw createError(500, "user not found");
+				}
+			} else {
+				throw createError(500, "your email isn't exsist");
+			}
+			resolve(userData);
+		} catch (er) {
+			reject(er);
+		}
+	});
+};
 
-let createUserAccount=(dataRegister)=>{
-    return new Promise(async(resolve,reject)=>{
-        try{
-            let userData={};
-            let isExsit=await checkUserEmail(dataRegister.email);//kiem tra email co ton tai
-            if(isExsit){
-                userData.errCode=1;
-                userData.errMessage="Your email has already exsited"
-            }else{
-                let hashPasswordFromBcrypt=await hashUserPassword(dataRegister.password);
-                await Account.create({
-                    email:dataRegister.email,
-                    password:hashPasswordFromBcrypt,
-                    role:dataRegister.role,
-                    demographic_id:dataRegister.demographic_id
-                })
-                userData.errCode=0;
-                userData.errMessage="Ok";
-                delete dataRegister.password;
-                userData.user=dataRegister;
-            }
-            resolve(userData);
-        }catch(er){
-            reject(er);
-        }
-    })
-}
-let checkUserEmail=async(userEmail)=>{
-    return new Promise(async(resolve,reject)=>{
-        try{
-            let user=await Account.findOne({
-                where:{email: userEmail}
-            })
-            if(user){
-                resolve(true);
-            }else{
-                resolve(false);
-            }
-        }catch(err){
-            reject(err);
-        }
-    })
-}
-let hashUserPassword=(password)=>{
-    return new Promise(async(resolve,reject)=>{
-        try{
-            let hashPassword=await bcrypt.hashSync(password,salt);
-            resolve(hashPassword);
-        }catch(ex){
-            reject(ex);
-        }
-    })
-}
-module.exports={//luu y jo duong exports={handleUserLogin:handleUserLogin}  vi se co lôi handleUserLogin is not function=>luc nao cung dung module.export
-    handleUserLogin:handleUserLogin,
-    createUserAccount:createUserAccount
-}
+let createUserAccount = (dataRegister) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let userData = {};
+			let isExsit = await checkUserEmail(dataRegister.email); //kiem tra email co ton tai
+			if (isExsit) {
+				throw createError(500, "Your email has already exsited");
+			} else {
+				let hashPasswordFromBcrypt = await hashUserPassword(
+					dataRegister.password
+				);
+				await Account.create({
+					email: dataRegister.email,
+					password: hashPasswordFromBcrypt,
+					role: dataRegister.role,
+					demographic_id: dataRegister.demographic_id,
+				});
+				userData.errMessage = "Register successful";
+				delete dataRegister.password;
+				userData.user = dataRegister;
+			}
+			resolve(userData);
+		} catch (er) {
+			reject(er);
+		}
+	});
+};
+let checkUserEmail = async (userEmail) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let user = await Account.findOne({
+				where: { email: userEmail },
+			});
+			if (user) {
+				resolve(true);
+			} else {
+				resolve(false);
+			}
+		} catch (err) {
+			reject(err);
+		}
+	});
+};
+let hashUserPassword = (password) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let hashPassword = await bcrypt.hashSync(password, salt);
+			resolve(hashPassword);
+		} catch (ex) {
+			reject(ex);
+		}
+	});
+};
+module.exports = {
+	//luu y jo duong exports={handleUserLogin:handleUserLogin}  vi se co lôi handleUserLogin is not function=>luc nao cung dung module.export
+	handleUserLogin,
+	createUserAccount,
+};
