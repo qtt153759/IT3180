8248f67ba5391ca5cf105be9706c0d333caf808c
qtt153759 <qtt153759@gmail.com>
2021-12-02 16:49:53 +0700

    Chuyen ve process.env trong config.js va trong package.json xoa phan thua tu dong 17->31

--- api/config/config.js
+++ api/config/config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    /** DATABASE */
-    db: {
-        DB_NAME: "quanlydancu",
-        DB_USER: "root",
-        DB_PASS: null,//process.env.DB_PASSWORD
-        DB_HOST: process.env.DB_HOST,
-        dialect: "mysql",
+	/** DATABASE */
+	db: {
+		DB_NAME: process.env.DB_NAME,
+		DB_USER: process.env.DB_USER,
+		DB_PASS: process.env.DB_PASS, //process.env.DB_PASSWORD
+		DB_HOST: process.env.DB_HOST,
+		dialect: "mysql",
 
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    },
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+	},
 
-    /** AUTH KEY */
-    auth: {
-        secret: "key"
-    }
+	/** AUTH KEY */
+	auth: {
+		secret: "key",
+	},
 };
