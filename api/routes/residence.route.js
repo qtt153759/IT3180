53c8029b957b60b46cd68f2c9dbbfac2c3bfb028
qtt153759 <qtt153759@gmail.com>
2021-12-12 16:45:38 +0700

    get demographic and residence by id

--- api/routes/residence.route.js
+++ api/routes/residence.route.js
@@ -4,6 +4,7 @@ const residenceController = require("../controllers/residence.controller");
 
 route.get("/", residenceController.getAll);
 route.post("/", residenceController.create);
+route.get("/:id", residenceController.getResidenceById);
 route.put("/update", residenceController.update);
 route.delete("/delete/:id", residenceController.deleteResidence);
 
