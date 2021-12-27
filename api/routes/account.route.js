fdb31e79fb2f1f87e192d12783e7b2d9d21eaf75
HuuDoanh <nhdoanh3012@gmail.com>
2021-12-27 21:47:28 +0700

    [doanhnh] update authen, author

--- api/routes/account.route.js
+++ api/routes/account.route.js
@@ -2,12 +2,11 @@ const express = require("express");
 
 const accountController = require("../controllers/account.controller");
 const route = express.Router();
-const author = require("../middlewares/author");
 const authen = require("../middlewares/authen");
 
-route.get("/", authen, author([]), accountController.getAllAccount);
+route.get("/", authen, accountController.getAllAccount);
 route.post("/login", accountController.handleLogin);
-route.post("/register", authen, author([]), accountController.createAccount);
-route.get("/profile", authen, author([]), accountController.getProfile);
+route.post("/register", authen, accountController.createAccount);
+route.get("/profile", authen, accountController.getProfile);
 
 module.exports = route;
